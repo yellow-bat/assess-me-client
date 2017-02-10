d59e1d6f786bdbfef549eb23fbedc1156851a703
Viktar_Parashchanka <viktar_parashchanka@epam.com>
2017-02-10 10:39:42 +0300

    Added skeleton of UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { LandingComponent } from './containers/LandingPage/landing.component';
+import { LayoutComponent } from './common/layout/layout.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { RegisterComponent } from './containers/RegisterPage/register.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: LandingComponent,
+    pathMatch: 'full'
+  },{
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    LayoutComponent,
+    LandingComponent,
+    FooterComponent,
+    HeaderComponent,
+    RegisterComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LayoutComponent]
 })
 export class AppModule { }
