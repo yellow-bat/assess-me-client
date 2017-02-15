f4e0056c34e31463d4a6cb538af4d3c00642be30
scolt <gxvint@gmail.com>
2017-02-15 12:52:44 +0300

    Added dynamically menu for header

--- src/app/common/components/header/header.component.ts
+++ src/app/common/components/header/header.component.ts
@@ -1,16 +1,39 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from '../../services/user.service';
 
+export interface MenuItem {
+  label: string;
+  url: string;
+  isPublic: boolean;
+}
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  defaultMenu: MenuItem[] = [{
+    label: 'Sign In',
+    url: '/login',
+    isPublic: true
+  },{
+    label: 'Register',
+    url: '/register',
+    isPublic: true
+  }, {
+    label: 'Logout',
+    url: '/logout',
+    isPublic: false
+  }];
+
+  menu: MenuItem[];
 
   constructor(private userService: UserService) {
     this.userService.isAuthenticated.subscribe((isAuthenticated: boolean) => {
-      console.log('subscribe: ', isAuthenticated);
+      this.menu = this.defaultMenu.filter( item =>
+        item.isPublic === !isAuthenticated
+      );
     });
   }
 
