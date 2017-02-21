aa05851cc0ca1f07e5edc30f478aed74745d5d70
Dzmitry.Shaplyka <dmitryshaplyko@gmail.com>
2017-02-21 11:53:25 +0300

    Test cases revised. All are stable now

--- e2e/tests/homePage.e2e-spec.ts
+++ e2e/tests/homePage.e2e-spec.ts
@@ -1,25 +1,29 @@
 import {HomePage} from "../pages/homePage";
 
-describe('Accessing Home page', function() {
+describe('Accessing Home page',() => {
   let homePage = new HomePage();
 
-  describe('When visiting Home Page', function() {
-    homePage.navigateToHome();
 
 
-    it('Should contain sign In button', function () {
+  describe('When visiting Home Page', () => {
+    beforeAll(() => {
+      homePage.navigateToHome();
+    });
+
+    it('Should contain sign In button', () => {
       expect(homePage.signInButton.isPresent());
     });
 
-    it('Should contain register button', function () {
+    it('Should contain register button', () => {
       expect(homePage.registerButton.isPresent);
     });
 
-    it('Should contain Logo', function () {
+    it('Should contain Logo', () => {
       expect(homePage.logo.isPresent());
     });
 
-    it('Should contain Welcome Text', function () {
+    it('Should contain Welcome Text', () =>  {
+
       expect(homePage.getWelcomeText()).toMatch('landing works!');
     });
   });
